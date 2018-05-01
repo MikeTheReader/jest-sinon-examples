40c2bc006aa95e1f43fe2b46ea774ce376265b1b
Michael Lundin <mikel@ifit.com>
2018-05-01 08:04:43 -0700

    Use correct mockRestore function

--- test/stubs-alternative-jest.test.js
+++ test/stubs-alternative-jest.test.js
@@ -4,10 +4,11 @@ import { getGoogleHomePage } from '../src/google';
 describe('Jest', () => {
   describe('Stubs - Using Spies', () => {
     it('returns the correct response', () => {
-      jest.spyOn(axios, 'get').mockResolvedValue({ name: 'Bob' });
+      const getSpy = jest.spyOn(axios, 'get');
+      getSpy.mockResolvedValue({ name: 'Bob' });
       return getGoogleHomePage().then((response) => {
         expect(response).toEqual({ name: 'Bob' });
-        axios.get.mockRestore();
+        getSpy.mockRestore();
       });
     });
   });
