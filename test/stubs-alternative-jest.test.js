4b37d1656ab048da42d12572f44c456dfe9bb195
Michael Lundin <mikel@ifit.com>
2018-05-01 08:26:57 -0700

    Change variable name

--- test/stubs-alternative-jest.test.js
+++ test/stubs-alternative-jest.test.js
@@ -4,11 +4,11 @@ import { getGoogleHomePage } from '../src/google';
 describe('Jest', () => {
   describe('Stubs - Using Spies', () => {
     it('returns the correct response', () => {
-      const getSpy = jest.spyOn(axios, 'get');
-      getSpy.mockResolvedValue({ name: 'Bob' });
+      const spyOnGet = jest.spyOn(axios, 'get');
+      spyOnGet.mockResolvedValue({ name: 'Bob' });
       return getGoogleHomePage().then((response) => {
         expect(response).toEqual({ name: 'Bob' });
-        getSpy.mockRestore();
+        spyOnGet.mockRestore();
       });
     });
   });
